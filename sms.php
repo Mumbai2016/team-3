993dfda3edb858ef8539b726eaba3295ed50054a
yash shah <shahyash96.ys@gmail.com>
2016-07-23 20:26:17 +0530

    Sms Sending Feature

---
+++ sms.php
@@ -0,0 +1,4 @@
+<?php
+    include('dev.sms.com/way2sms-api.php');
+    sendWay2SMS ( '9920902322' , 'E6475E' , '9920350257' , 'This is to remind you that you have lectures pending for the ongoing month.Check website for further details.');   
+?>
