f9224772d03fcd94ee280dc5f4b66879e50874f6
Shantanu Ugemuge <shantanuugemuge@gmail.com>
2016-07-24 04:48:24 +0530

    Update main.php

--- chat/main.php
+++ chat/main.php
@@ -1,9 +1,5 @@
 <?php
 
-// original article url
-// http://www.script-tutorials.com/how-to-easily-make-a-php-chat-application
-
-// set error reporting level
 if (version_compare(phpversion(), "5.3.0", ">=") == 1)
   error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED);
 else
