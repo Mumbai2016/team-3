f53f6980e078082c8678b006aa47ae786b884281
yash shah <shahyash96.ys@gmail.com>
2016-07-24 06:30:47 +0530

    Minor DB change

--- katalyst_team3.sql
+++ katalyst_team3.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.0.10deb1
+-- version 4.4.14
 -- http://www.phpmyadmin.net
 --
--- Host: localhost
--- Generation Time: Jul 23, 2016 at 11:07 PM
--- Server version: 5.5.50-0ubuntu0.14.04.1
--- PHP Version: 5.5.9-1ubuntu4.17
+-- Host: 127.0.0.1
+-- Generation Time: Jul 24, 2016 at 02:59 AM
+-- Server version: 5.6.26
+-- PHP Version: 5.6.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,7 +14,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `katalyst_team3`
@@ -27,11 +27,10 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `admin` (
-  `adm_id` int(11) NOT NULL AUTO_INCREMENT,
+  `adm_id` int(11) NOT NULL,
   `adm_email` varchar(50) NOT NULL,
-  `adm_pass` varchar(40) NOT NULL,
-  PRIMARY KEY (`adm_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=11 ;
+  `adm_pass` varchar(40) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `admin`
@@ -48,8 +47,7 @@ INSERT INTO `admin` (`adm_id`, `adm_email`, `adm_pass`) VALUES
 --
 -- Triggers `admin`
 --
-DROP TRIGGER IF EXISTS `admin_to_login`;
-DELIMITER //
+DELIMITER $$
 CREATE TRIGGER `admin_to_login` AFTER INSERT ON `admin`
  FOR EACH ROW BEGIN
     INSERT INTO login (log_email,log_pass,log_role) 
@@ -57,7 +55,7 @@ CREATE TRIGGER `admin_to_login` AFTER INSERT ON `admin`
     FROM admin ORDER BY adm_id DESC 
     LIMIT 1;
 END
-//
+$$
 DELIMITER ;
 
 -- --------------------------------------------------------
@@ -67,13 +65,10 @@ DELIMITER ;
 --
 
 CREATE TABLE IF NOT EXISTS `assignment` (
-  `assign_id` int(11) NOT NULL AUTO_INCREMENT,
+  `assign_id` int(11) NOT NULL,
   `assign_mtrid` int(11) NOT NULL,
-  `assign_mntid` int(11) NOT NULL,
-  PRIMARY KEY (`assign_id`),
-  KEY `assign_mtrid` (`assign_mtrid`),
-  KEY `assign_mntid` (`assign_mntid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+  `assign_mntid` int(11) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -82,12 +77,11 @@ CREATE TABLE IF NOT EXISTS `assignment` (
 --
 
 CREATE TABLE IF NOT EXISTS `login` (
-  `log_id` int(11) NOT NULL AUTO_INCREMENT,
+  `log_id` int(11) NOT NULL,
   `log_email` varchar(50) NOT NULL,
   `log_pass` varchar(40) NOT NULL,
-  `log_role` varchar(10) NOT NULL,
-  PRIMARY KEY (`log_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=13 ;
+  `log_role` varchar(10) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `login`
@@ -114,14 +108,13 @@ INSERT INTO `login` (`log_id`, `log_email`, `log_pass`, `log_role`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `meetings_comp` (
-  `mcom_id` int(11) NOT NULL AUTO_INCREMENT,
+  `mcom_id` int(11) NOT NULL,
   `mcom_mtrid` int(11) NOT NULL,
   `mcom_mntid` int(11) NOT NULL,
   `mcom_date` varchar(15) NOT NULL,
   `mcom_hrs` varchar(15) NOT NULL,
-  `mcom_loc` varchar(120) NOT NULL,
-  PRIMARY KEY (`mcom_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+  `mcom_loc` varchar(120) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -130,15 +123,22 @@ CREATE TABLE IF NOT EXISTS `meetings_comp` (
 --
 
 CREATE TABLE IF NOT EXISTS `meetings_sche` (
-  `msce_id` int(11) NOT NULL AUTO_INCREMENT,
+  `msce_id` int(11) NOT NULL,
   `msce_mtrid` int(11) NOT NULL,
   `msce_mntid` int(11) NOT NULL,
   `msce_date` varchar(15) NOT NULL,
-  `msce_time` varchar(15) NOT NULL,
   `msce_loc` varchar(120) NOT NULL,
   `msce_timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`msce_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+  `status` int(11) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `meetings_sche`
+--
+
+INSERT INTO `meetings_sche` (`msce_id`, `msce_mtrid`, `msce_mntid`, `msce_date`, `msce_loc`, `msce_timestamp`, `status`) VALUES
+(1, 1, 1, '2016-07-27', 'andheri', '2016-07-24 00:33:34', 0),
+(2, 0, 1, '2016-07-28', 'goregaon', '2016-07-24 00:48:43', 0);
 
 -- --------------------------------------------------------
 
@@ -147,7 +147,7 @@ CREATE TABLE IF NOT EXISTS `meetings_sche` (
 --
 
 CREATE TABLE IF NOT EXISTS `mentee_basic` (
-  `mnt_id` int(11) NOT NULL AUTO_INCREMENT,
+  `mnt_id` int(11) NOT NULL,
   `mnt_name` varchar(40) NOT NULL,
   `mnt_dob` date NOT NULL,
   `mnt_address` varchar(120) NOT NULL,
@@ -157,16 +157,15 @@ CREATE TABLE IF NOT EXISTS `mentee_basic` (
   `mnt_resno` varchar(15) DEFAULT NULL,
   `mnt_email` varchar(50) NOT NULL,
   `mnt_pass` varchar(40) NOT NULL,
-  `mnt_aggr12th` varchar(7) NOT NULL,
-  PRIMARY KEY (`mnt_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=6 ;
+  `mnt_aggr12th` varchar(7) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `mentee_basic`
 --
 
 INSERT INTO `mentee_basic` (`mnt_id`, `mnt_name`, `mnt_dob`, `mnt_address`, `mnt_college`, `mnt_cyear`, `mnt_mobile`, `mnt_resno`, `mnt_email`, `mnt_pass`, `mnt_aggr12th`) VALUES
-(1, 'Jane Rogers', '1995-01-17', '123 buidling street andheri mumbai', 'MIT', '4th', '9876543210', '022-2544323', 'shantanu1938@gmail.com', 'key123', '80%'),
+(1, 'Jane Rogers', '1995-01-17', '123 buidling street andheri mumbai', 'MIT', '4th', '9920350257', '022-2544323', 'tanawde78@gmail.com', 'key123', '80%'),
 (2, 'Rucha Shah', '1996-07-08', 'Jai Prakash Rd, Sahakar Nagar, Azad Nagar, Andheri West, Mumbai', 'VESIT', '3rd', '9876152364', '022-2348695', 'yogeetamanglani@gmail.com', 'key1234', '90%'),
 (3, 'Sweta Gupta', '1995-11-04', '1,S.N.D.T College,Nathibai Thackersey Road,Juhu, Mumbai', 'S.N.D.T College', '4th Year', '9876543092', NULL, 'swetagupta19@gmail.com', 'key123', '60%'),
 (4, 'Richa Shah', '1994-07-20', 'Plot No.U-15, J.V.P.D. Scheme, Bhaktivedanta Swami Marg, Vile Parle West,Mumbai', 'D.J Sanghvi', '4th Year', '8879444333', '022-23456433', 'richashah@gmail.com', 'key123', '75%'),
@@ -175,8 +174,7 @@ INSERT INTO `mentee_basic` (`mnt_id`, `mnt_name`, `mnt_dob`, `mnt_address`, `mnt
 --
 -- Triggers `mentee_basic`
 --
-DROP TRIGGER IF EXISTS `mentee_to_login`;
-DELIMITER //
+DELIMITER $$
 CREATE TRIGGER `mentee_to_login` AFTER INSERT ON `mentee_basic`
  FOR EACH ROW BEGIN
     INSERT INTO login (log_email,log_pass,log_role) 
@@ -184,7 +182,7 @@ CREATE TRIGGER `mentee_to_login` AFTER INSERT ON `mentee_basic`
     FROM mentee_basic ORDER BY mnt_id DESC 
     LIMIT 1;
 END
-//
+$$
 DELIMITER ;
 
 -- --------------------------------------------------------
@@ -194,7 +192,7 @@ DELIMITER ;
 --
 
 CREATE TABLE IF NOT EXISTS `mentor_basic` (
-  `mtr_id` int(11) NOT NULL AUTO_INCREMENT,
+  `mtr_id` int(11) NOT NULL,
   `mtr_email` varchar(50) NOT NULL,
   `mtr_pass` varchar(40) NOT NULL,
   `mtr_name` varchar(50) NOT NULL,
@@ -206,9 +204,8 @@ CREATE TABLE IF NOT EXISTS `mentor_basic` (
   `mtr_HighAcadQual` varchar(20) NOT NULL,
   `mtr_currentcom` varchar(40) NOT NULL,
   `mtr_workex` int(3) NOT NULL,
-  `mtr_status` int(11) DEFAULT NULL,
-  PRIMARY KEY (`mtr_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
+  `mtr_status` int(11) DEFAULT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `mentor_basic`
@@ -223,8 +220,7 @@ INSERT INTO `mentor_basic` (`mtr_id`, `mtr_email`, `mtr_pass`, `mtr_name`, `mtr_
 --
 -- Triggers `mentor_basic`
 --
-DROP TRIGGER IF EXISTS `mentor_to_login`;
-DELIMITER //
+DELIMITER $$
 CREATE TRIGGER `mentor_to_login` AFTER INSERT ON `mentor_basic`
  FOR EACH ROW BEGIN
     INSERT INTO login (log_email,log_pass,log_role) 
@@ -232,7 +228,7 @@ CREATE TRIGGER `mentor_to_login` AFTER INSERT ON `mentor_basic`
     FROM mentor_basic ORDER BY mtr_id DESC 
     LIMIT 1;
 END
-//
+$$
 DELIMITER ;
 
 -- --------------------------------------------------------
@@ -242,14 +238,10 @@ DELIMITER ;
 --
 
 CREATE TABLE IF NOT EXISTS `mentor_skills` (
-  `ms_id` int(11) NOT NULL AUTO_INCREMENT,
+  `ms_id` int(11) NOT NULL,
   `ms_mtrid` int(10) NOT NULL,
-  `ms_skillid` int(10) NOT NULL,
-  PRIMARY KEY (`ms_id`),
-  KEY `ms_mtrid` (`ms_mtrid`),
-  KEY `ms_mtrid_2` (`ms_mtrid`),
-  KEY `ms_mtrid_3` (`ms_mtrid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+  `ms_skillid` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -258,13 +250,12 @@ CREATE TABLE IF NOT EXISTS `mentor_skills` (
 --
 
 CREATE TABLE IF NOT EXISTS `progress_mentee` (
-  `prog_id` int(11) NOT NULL AUTO_INCREMENT,
+  `prog_id` int(11) NOT NULL,
   `prog_mntid` int(11) NOT NULL,
   `prog_hrs` int(11) NOT NULL DEFAULT '0',
   `prog_meetingno` int(11) NOT NULL DEFAULT '0',
-  `prog_month` varchar(20) NOT NULL,
-  PRIMARY KEY (`prog_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+  `prog_month` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -273,10 +264,9 @@ CREATE TABLE IF NOT EXISTS `progress_mentee` (
 --
 
 CREATE TABLE IF NOT EXISTS `skills_master` (
-  `skill_id` int(11) NOT NULL AUTO_INCREMENT,
-  `skill_name` varchar(15) NOT NULL,
-  PRIMARY KEY (`skill_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
+  `skill_id` int(11) NOT NULL,
+  `skill_name` varchar(15) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `skills_master`
@@ -294,13 +284,146 @@ INSERT INTO `skills_master` (`skill_id`, `skill_name`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `s_chat_messages` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `user` varchar(255) NOT NULL,
   `message` varchar(255) NOT NULL,
-  `when` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+  `when` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`adm_id`);
+
+--
+-- Indexes for table `assignment`
+--
+ALTER TABLE `assignment`
+  ADD PRIMARY KEY (`assign_id`),
+  ADD KEY `assign_mtrid` (`assign_mtrid`),
+  ADD KEY `assign_mntid` (`assign_mntid`);
+
+--
+-- Indexes for table `login`
+--
+ALTER TABLE `login`
+  ADD PRIMARY KEY (`log_id`);
+
+--
+-- Indexes for table `meetings_comp`
+--
+ALTER TABLE `meetings_comp`
+  ADD PRIMARY KEY (`mcom_id`);
+
+--
+-- Indexes for table `meetings_sche`
+--
+ALTER TABLE `meetings_sche`
+  ADD PRIMARY KEY (`msce_id`);
+
+--
+-- Indexes for table `mentee_basic`
+--
+ALTER TABLE `mentee_basic`
+  ADD PRIMARY KEY (`mnt_id`);
+
+--
+-- Indexes for table `mentor_basic`
+--
+ALTER TABLE `mentor_basic`
+  ADD PRIMARY KEY (`mtr_id`);
+
+--
+-- Indexes for table `mentor_skills`
+--
+ALTER TABLE `mentor_skills`
+  ADD PRIMARY KEY (`ms_id`),
+  ADD KEY `ms_mtrid` (`ms_mtrid`),
+  ADD KEY `ms_mtrid_2` (`ms_mtrid`),
+  ADD KEY `ms_mtrid_3` (`ms_mtrid`);
+
+--
+-- Indexes for table `progress_mentee`
+--
+ALTER TABLE `progress_mentee`
+  ADD PRIMARY KEY (`prog_id`);
+
+--
+-- Indexes for table `skills_master`
+--
+ALTER TABLE `skills_master`
+  ADD PRIMARY KEY (`skill_id`);
+
+--
+-- Indexes for table `s_chat_messages`
+--
+ALTER TABLE `s_chat_messages`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `adm_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=11;
+--
+-- AUTO_INCREMENT for table `assignment`
+--
+ALTER TABLE `assignment`
+  MODIFY `assign_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `login`
+--
+ALTER TABLE `login`
+  MODIFY `log_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=13;
+--
+-- AUTO_INCREMENT for table `meetings_comp`
+--
+ALTER TABLE `meetings_comp`
+  MODIFY `mcom_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `meetings_sche`
+--
+ALTER TABLE `meetings_sche`
+  MODIFY `msce_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `mentee_basic`
+--
+ALTER TABLE `mentee_basic`
+  MODIFY `mnt_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
+--
+-- AUTO_INCREMENT for table `mentor_basic`
+--
+ALTER TABLE `mentor_basic`
+  MODIFY `mtr_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT for table `mentor_skills`
+--
+ALTER TABLE `mentor_skills`
+  MODIFY `ms_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `progress_mentee`
+--
+ALTER TABLE `progress_mentee`
+  MODIFY `prog_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `skills_master`
+--
+ALTER TABLE `skills_master`
+  MODIFY `skill_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `s_chat_messages`
+--
+ALTER TABLE `s_chat_messages`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- Constraints for dumped tables
 --
